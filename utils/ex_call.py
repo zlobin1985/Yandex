680b7c67b123f60edc375c8f8dc09a8f30fad5a4
zlobin1985 <zlobin@teamidea.ru>
2026-01-29 14:45:40 +0000

    2026-01-29 - вызов spark сессии

--- utils/ex_call.py
+++ utils/ex_call.py
@@ -1,12 +1,6 @@
 from ex_def import get_session
 
-# Задаём значение аргумента a
-a = 51
-
-# Вызываем функцию и сохраняем результат
-#result = get_session(
-
-# Выводим результат
+# Открываем сессию spark и показываем таблицу
 spark = get_session()
 
 df = spark.sql("SELECT * FROM my_catalog.my_table")
